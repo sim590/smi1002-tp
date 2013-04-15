da7732a8d9dd1d9cb23fabfa9daf6fbc1eeb8e8f
Simon Desaulniers <rostydela@gmail.com>
2013-04-14 21:49:23 -0400

    (WIP)gestion de l'ajout d'un TypePersonne. Ajout des classes Personnel, TypePersonnel, DonneeBD (abstraite)

--- src/TP_SMI1002/FormTypePersonnel.cs
+++ src/TP_SMI1002/FormTypePersonnel.cs
@@ -6,11 +6,13 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace TP_SMI1002
 {
     public partial class FormTypePersonnel : Form
     {
+        // Constructeur
         public FormTypePersonnel()
         {
             InitializeComponent();
@@ -24,11 +26,31 @@ namespace TP_SMI1002
             }
         }
 
+        // Enregistrer un type de personnel
         private void btnEnregistrer_Click(object sender, EventArgs e)
         {
-
+            if (pbCouleur.BackColor != null && estNomValide(this.txtNom.Text))
+            {
+                TypePersonnel type = new TypePersonnel(pbCouleur.BackColor.A, pbCouleur.BackColor.R,
+                    pbCouleur.BackColor.G,pbCouleur.BackColor.B,this.txtNom.Text);
+                
+                /*
+                 * //TODO: DEMANDER AU SINGLETON (AVEC PATRON FABRIQUE) D'AJOUTER LA DONNÉE DANS LA BD
+                 */
+            }
         }
 
+        // Vérifie que le nom entré est vrai-semblablement un nom
+        private bool estNomValide(string nom)
+        {
+            // Première vérification
+            foreach (char c in nom)
+                if (c != ' ' && Regex.IsMatch(nom, @"^[a-zA-Z]+$"))
+                    return true;
+            return false;
+        }
+
+        // Annuler la transaction
         private void btnAnnuler_Click(object sender, EventArgs e)
         {
             this.Close();
