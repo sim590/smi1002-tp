ca6e37eb534fa6cadc38048cf38ffb08e7dff790
Jordan Blanchette <blanchette.jordan@gmail.com>
2013-04-25 22:51:10 -0400

    heritage sur TypeJeu et suppression de IDs

--- src/TP_SMI1002/TypeJeu.cs
+++ src/TP_SMI1002/TypeJeu.cs
@@ -5,16 +5,17 @@ using System.Text;
 
 namespace TP_SMI1002
 {
-    class TypeJeu
+    class TypeJeu :DonneeBD
     {
-        int ID{get; set;}
         string NomTypeJeu {get; set;}
-        public TypeJeu(int ID, string NomTypeJeu)
+
+        //constructeur
+        public TypeJeu(string NomTypeJeu)
         {
-            this.ID = ID;
             this.NomTypeJeu = NomTypeJeu;
         }
 
+        //override ToString
         public override string ToString()
         {
             return this.NomTypeJeu;
