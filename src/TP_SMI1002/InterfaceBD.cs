9f4734e9ed22164875826cc0946db0b1919aab25
Jordan Blanchette <blanchette.jordan@gmail.com>
2013-04-25 23:51:21 -0400

    Ajout des suppressions sur la base de Donnees

--- src/TP_SMI1002/InterfaceBD.cs
+++ src/TP_SMI1002/InterfaceBD.cs
@@ -86,6 +86,70 @@ namespace TP_SMI1002
 
         }
 
-        public int supprimmerDansBD
+        public int supprimmerDansBD(DonneeBD donnee)
+        {
+            OracleCommand cmd = new OracleCommand(); // fournir objet OracleConnection et le string de commande
+            string cmdString;
+
+            cnLanUQTR.Open();
+
+            //suppression d'un objet Personnel
+            if (donnee is Personnel)
+            {
+                cmdString = @"DELETE FROM PERSONNEL WHERE ID=" + ((Personnel)donnee).Id;   
+            }
+            //suppression d'un objet TypePersonnel
+            else if (donnee is TypePersonnel)
+            {
+                cmdString = @"DELETE FROM TYPEPERSONNEL WHERE ID=" + ((TypePersonnel)donnee).Id;
+            }
+            //suppression d'un objet Equipe
+            else if (donnee is Equipe)
+            {
+                cmdString = @"DELETE FROM EQUIPE WHERE ID=" + ((Equipe)donnee).Id;
+            }
+            //suppression d'un objet Joueur
+            else if (donnee is Joueur)
+            {
+                cmdString = @"DELETE FROM JOUEUR WHERE ID=" + ((Joueur)donnee).Id;
+            }
+            //suppression d'un objet Jeu
+            else if (donnee is Jeu)
+            {
+                cmdString = @"DELETE FROM JEU WHERE ID=" + ((Jeu)donnee).Id;
+            }
+            //suppression d'un objet TypeJeu
+            else if (donnee is TypeJeu)
+            {
+                cmdString = @"DELETE FROM TYPEJEU WHERE ID=" + ((TypeJeu)donnee).Id;
+            }
+            //suppression d'un objet Evenement
+            else if (donnee is Evenement)
+            {
+                cmdString = @"DELETE FROM EVENEMENT WHERE ID=" + ((Evenement)donnee).Id;
+            }
+            //suppression d'un objet Tournoi
+            else if (donnee is Tournoi)
+            {
+                cmdString = @"DELETE FROM TOURNOI WHERE ID=" + ((Tournoi)donnee).Id;
+            }
+            //suppression d'un objet Evenement
+            else if (donnee is Evenement)
+            {
+                cmdString = @"DELETE FROM EVENEMENT WHERE ID=" + ((Evenement)donnee).Id;
+            }
+
+
+            // Envoie la commande
+            cmd.Connection=cnLanUQTR;
+
+            cmd.CommandText = cmdString;
+
+            cmd.ExecuteNonquery();
+
+            // Fermeture de la connexion
+            cnLanUQTR.Close();
+        }
+
     }
 }
